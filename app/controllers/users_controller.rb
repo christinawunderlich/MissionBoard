567f71f94bf6eda290ec8d67f1459515657dc621
Christina Wunderlich <christina.wunderlich@gmail.com>
2017-02-05 23:15:27 +0100

    add boards controller and link users to mission board view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
   def index
   	@users = User.all
+
+    @user = User.new
+
   end
+
+  def create
+  	@user = User.new(user_params)
+
+  	respond_to do |format|
+  		if @user.save
+  		  format.html { redirect_to boards_url , notice: 'User was successfully created.' }
+      else 
+        format.html { render :index}
+      end
+    end
+  end
+
+end
+
+
+private
+
+def user_params
+	params.require(:user).permit(:name)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index]
+  resources :users, only: [:index, :create]
+  resources :boards, only: [:index]
 
 end
