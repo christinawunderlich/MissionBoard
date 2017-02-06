ae2333632aee878a7d27278b068e02b59e386c59
Christina Wunderlich <christina.wunderlich@gmail.com>
2017-02-06 12:32:57 +0100

    update user form and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,24 @@
 class UsersController < ApplicationController
   def index
   	@users = User.all
-
     @user = User.new
-
   end
 
   def create
   	@user = User.new(user_params)
 
-  	respond_to do |format|
-  		if @user.save
-  		  format.html { redirect_to boards_url , notice: 'User was successfully created.' }
-      else 
-        format.html { render :index}
-      end
+		if @user.save
+      redirect_to board_path(user_id: @user.id)
+    else 
+      render :index, notice: 'User cannot be empty'
     end
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:name)
   end
 
 end
 
-
-private
-
-def user_params
-	params.require(:user).permit(:name)
-end
